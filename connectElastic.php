4f51715ea993e805300c54c8d439a71ff6fd860c
rinuma-dev <rinumadev.mail@gmail.com>
2021-08-20 11:03:07 +0700

    Elastic function bulk indexing

--- connectElastic.php
+++ connectElastic.php
@@ -8,65 +8,96 @@ require './connectRedis.php';
 $JsonUwsgi = file_get_contents("json/uwsgi.json");
 $JsonUwsgi = json_decode($JsonUwsgi, true);
 
-$client = ClientBuilder::create()->build();
+$clientElastic = ClientBuilder::create()->build();
 
-function indexingUwsgiElastic($client, $json)
+function indexingJsonElastic($client, $json)
 {
-    $index= 0;
-    $params = array('body'=>array());
+    $index = 0;
+    $params = array('body' => array());
     foreach ($json as $data) {
-        // var_dump($data);
+        echo "start function";
+        
         // var_dump($data['rss']);
         $params['body'][] = array(
             'index' => array(
-                '_index'=>'log_uwsgi',
-                '_type'=>'log_uwsgi',
-                '_id'=>$data['id']
+                '_index' => 'log_uwsgi_3',
+                '_type' => 'log_uwsgi_3',
+                '_id' => $data['id']
             )
         );
 
-        $params['body'][]=array(
-            'address_space_usage'=>$data['address_space_usage'],
-            'address_space' =>$data['address_space'],
-            'rss_usage'=>$data['rss_usage'],
-            'rss'=>$data['rss'],
-            'pid'=>$data['pid']
+        $params['body'][] = array(
+            'address_space_usage' => $data['address_space_usage'],
+            'address_space' => $data['address_space'],
+            'rss_usage' => $data['rss_usage'],
+            'rss' => $data['rss'],
+            'pid' => $data['pid']
         );
-        if($index % 100 === 0){
+        if ($index % 100 === 0) {
+            echo "params on bulk";
+            // var_dump($params);
             $response = $client->bulk($params);
             // print_r($response);
             $params = array();
             unset($response);
         }
-        $index+= $index; 
-        
+        $index += $index;
+
         // print_r($response);
     };
+    // var_dump($params);
 }
 
-function getDataByid( $client, $index, $type, $id){
+function indexingToElastic($clientElastic,$data,$nameIndex)
+{
+    
+    // $params = array('body' => array());
+    $params['body'][] = array(
+        'index' => array(
+            '_index' => $nameIndex,
+            '_type' => $nameIndex,
+            '_id' => $data->id
+        )
+    );
+
+    $params['body'][] = array(
+        'address_space_usage' => $data->address_space_usage,
+        'address_space_used' => $data->address_space_used,
+        'address_space_size' => $data->address_space_size,
+        'rss_usage' => $data->rss_usage,
+        'rss_used' => $data->rss_used,
+        'rss_size' => $data->rss_size,
+        'pid' => $data->pid
+    );
+    $response = $clientElastic->bulk($params);
+    return $response;
+};
+
+function getDataByid($client, $index, $type, $id)
+{
     $params['index'] = $index;
     $params['type'] = $type;
-    $params['id'] = $id; 
+    $params['id'] = $id;
 
     $result = $client->get($params);
     // var_dump($result);
     return $result;
 };
 
-function getDatabySize($client, $index, $type, $size){
+function getDatabySize($client, $index, $type, $size)
+{
     $params = array();
     $params['index'] = $index;
     $params['type'] = $type;
     $params['size'] = $size;
-    $params['body']['query']['match_all']= new \stdClass();
-    
+    $params['body']['query']['match_all'] = new \stdClass();
+
     $result = $client->search($params);
     var_dump($result);
     return $result;
 };   
 
 
-// indexingUwsgiElastic($client, $JsonUwsgi);
-getDataByid($client,'log_uwsgi','log_uwsgi',405);
-getDatabySize($client,'log_uwsgi','log_uwsgi',100);
+// indexingUwsgiElastic($clientElastic, $JsonUwsgi);
+// getDataByid($client,'log_uwsgi','log_uwsgi',405);
+// getDatabySize($client,'log_uwsgi','log_uwsgi',100);
